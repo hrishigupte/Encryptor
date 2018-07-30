e95798c30a850a46abc3f759a6cccf3d78844ccc
Hrishi Gupte <hrishigupte@yahoo.com>
2018-07-29 21:42:02 -0400

    Publish script to compile on linux

--- publish.sh
+++ publish.sh
@@ -7,7 +7,7 @@ dotnet publish -c Release --self-contained -r opensuse-x64
 dotnet publish -c Release --self-contained -r osx-x64
 
 cd bin/Release/netcoreapp2.0
-tar -cvf Encryptor-ubuntu.16.04-x64 ubuntu.16.04-x64/*
-tar -cvf Encryptor-opensuse-x64 opensuse-x64/*
-zip -r Encryptor-win10-x64 win10-x64/*
-tar -cvf Encryptor-osx-x64 osx-x64/* 
+tar -cvf Encryptor-ubuntu.16.04-x64.tar ubuntu.16.04-x64/*
+tar -cvf Encryptor-opensuse-x64.tar opensuse-x64/*
+zip -r Encryptor-win10-x64.zip win10-x64/*
+tar -cvf Encryptor-osx-x64.tar osx-x64/* 
